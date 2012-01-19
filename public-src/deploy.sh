125693f677c447159d7197683768732aad11569d
AJ ONeal <coolaj86@gmail.com>
2012-01-19 17:29:38 +0000

    added output messages

--- public-src/deploy.sh
+++ public-src/deploy.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-jade index.jade
+echo "Rendering jade to html"
+jade index.jade > /dev/null
+echo "Transpiling less to css"
 lessc style.less > style.css
-pakmanager build
+echo "Packing EcmaScript"
+pakmanager build > /dev/null
 uglifyjs pakmanaged.js > app.min.js
+rm pakmanaged.*
 
+echo "Copying static assets"
 rsync -a static/ ../public/
 
+echo "Moving deployment to ../public"
 mv index.html style.css app.min.js ../public
+
+echo ""
+echo "Done"
