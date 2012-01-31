76287da3ddfd4bcd00e1894e8287daac8da22138
AJ ONeal <coolaj86@gmail.com>
2012-01-30 23:41:22 -0700

    properly builds and adds / removes config info

--- build.js
+++ build.js
@@ -6,29 +6,124 @@
     , walk = require('walk')
     , markdown = require('markdown').markdown
     , Highlight = require('highlight')
+    , forEachAsync = require('forEachAsync')
     , configPath = __dirname + '/config.yml'
     , srcPath = __dirname + '/src'
+    , path = require('path')
     , config = {}
     ;
 
   YAML.parse = require('yaml').eval;
 
-  function writeNewConfigFile() {
+  function writeNewConfigFile(cb) {
     var ymlText
+      , deleteGroups = []
       ;
 
-    ymlText = YAML.stringify(config) + '\n';
+    forEachAsync(config.groups, function (next, group, i) {
 
-    fs.writeFile(configPath, ymlText, 'utf8', function (err) {
-      if (err) {
-        console.error(err.stack);
+      if (!group.documents || !group.documents.length) {
+        config.groups[i] = null;
+        next();
+        return;
       }
+
+      forEachAsync(group.documents, function (next2, file, j) {
+        path.exists(srcPath + '/' + group.name + '/' + file.filename, function (exists) {
+          if (!exists) {
+            group.documents[j] = null;
+          }
+
+          next2();
+        });
+      }).then(function () {
+        group.documents = group.documents.filter(function (x) {
+          return !!x;
+        });
+
+        next();
+      });
+
+    }).then(function () {
+      config.groups = config.groups.filter(function (x) {
+        return !!x;
+      });
+
+      ymlText = YAML.stringify(config) + '\n';
+
+      fs.writeFile(configPath, ymlText, 'utf8', function (err) {
+        if (err) {
+          console.error(err.stack);
+        }
+
+        // TODO cb();
+      });
     });
   }
 
   function convertMarkdown(root, stat, next) {
     var fullpath = root + '/' + stat.name
+      , groupName
+      , newDoc = {
+            filename: stat.name
+          , title: stat.name.replace(/\.md$/, '')
+        }
       ;
+
+    function addToConfig(fileConfig) {
+      var foundDoc
+        , newConfig
+        , foundGroup
+        , docList = []
+        ;
+
+      groupName = root.slice(srcPath.length + 1);
+
+      newConfig = fileConfig || {};
+      newConfig.filename = stat.name;
+      newConfig.title = newConfig.title || stat.name.replace(/\.md$/, '');
+
+      config.groups.some(function (group) {
+        if (groupName !== group.name) {
+          return;
+        }
+
+        foundGroup = true;
+        docList = group.documents = group.documents || [];
+        return true;
+      });
+
+      docList.some(function (document, i) {
+        if (stat.name !== document.filename) {
+          return;
+        }
+
+        foundDoc = true;
+
+        // the data in the yml file or frontmatter
+        // should be preferred to data in the config
+        if (fileConfig) {
+          fileConfig.filename = newConfig.filename;
+          fileConfig.title = fileConfig.title || newConfig.title;
+          docList[i] = fileConfig;
+        }
+
+        return true;
+      });
+
+      // if there is no config data yet, some reasonable
+      // defaults will be added
+      if (!foundDoc) {
+        docList.push(newConfig);
+      }
+
+      if (!foundGroup) {
+        config.groups.push({
+            name: groupName
+          , documents: docList
+        });
+      }
+    }
 
     function checkWriteError(err) {
       if (err) {
@@ -47,14 +142,20 @@
       // TODO parse YAML front-matter, if any
       
       getYamlConfig(text, function (err, _text, frontMatter) {
-        console.log(frontMatter);
+        addToConfig(frontMatter);
+
         text = _text;
         // TODO where are the superflous `\n`s being introduced?
         text = markdown.toHTML(text);
         text = Highlight.highlight(text, '  ', true);
         // TODO
-        // for tpl in config.tpls
-        // text = text.replace(new RegExp(key, 'g'), val);
+        Object.keys(config.templates).forEach(function (key) {
+          var val = config.templates[key]
+            ;
+
+          text = text.replace(new RegExp('{{' + key + '}}', 'g'), val);
+        });
+
         fs.writeFile(fullpath.replace(/\.md$/, '.html'), text, 'utf8', checkWriteError);
       });
     }
@@ -124,11 +225,10 @@
           }
         }
 
-        cb(err, text, fileConfig || {});
+        cb(err, text, fileConfig);
       });
     }
 
-    console.log('name: ', stat.name);
     if (!stat.name.match(/\.md$/)) {
       next();
       return;
@@ -141,7 +241,6 @@
     var walker = walk.walk(srcPath)
       ;
 
-    console.log(srcPath);
     walker.on('file', convertMarkdown);
     walker.on('end', writeNewConfigFile);
   }
@@ -151,12 +250,12 @@
       if (err) {
         //console.error(configPath + ' could not be read.');
         //return;
-        text = '---\n  title: foo';
+        text = '---\n  title: Please Change Title\n';
       }
 
-      console.log('barish:', text);
       config = YAML.parse(text);
-      console.log('stuperfoo:', config);
+      config.groups = config.groups || [];
+      config.templates = config.templates || {};
       beginWalk();
     }
 
--- src/some-group/foo.md
+++ src/some-group/foo.md
@@ -5,10 +5,15 @@ title: yahoo
 foo
 ===
 
+{{testtpl}}
+
 pure awesomeness
 
+    {{testtpl}}
     (function () {
       "use strict";
       
       var x = "I'm JavaScript";
     }());
+
+{{testtpl}}
